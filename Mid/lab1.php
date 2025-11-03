7a7e2aea63758119e20597e420b5b215c4598545
unknown <ankon190@gmail.com>
2025-11-03 12:15:11 +0600

    registration form is created in lab

---
+++ Mid/lab1.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form page</title>
+</head>
+<body style="background-color:rgb(167, 176, 170)">
+    <div style="aling:center;background-color:rgba(246, 230, 241, 1); margin:200px; padding-left: 20px">
+        <h3 id="headiing" style="color:red">Student Registration Information</h3><br>
+        First Name <br>
+            <input type="text"> <br>
+        Last Name <br>
+            <input type="text"> <br>
+        Student ID <br>
+            <input type="number"> <br>
+        Program/Major <br>
+            <input type="text"> <br>
+        Department <br>
+           <input type="text"> <br>
+        Phone <br>
+            <input type="number"> <br>
+        University Email <br>
+            <input type="email"> <br>
+        Create Password (min 8 character) <br>
+            <input type="password"> <br>
+        Confirm password <br>
+            <input type="passwqord"> <br>
+        Semester selection <br>
+            <input type="radio">Summer 2024  
+            <input type="radio" >Fall 2024  
+            <input type="radio">Spring 2025 <br> 
+            <input type="radio">Other/Special term  
+        Required Credit Load(max 15) <br>
+            <input type="text"><br>
+        <input type="checkbox">I require academic advising before final Registration <br>
+
+        <h3 style="color:red">Course Selection</h3>
+            <input type="checkbox">MATH 1201 (Calculus) 
+            <input type="checkbox">CS 2105 (Data Structures) 
+            <input type="checkbox">ECON 1001 (Microeconomics)<br>
+            <input type="checkbox">PHY 1102 (Physics Lab) <br><br>
+        Comments/Special Requests<br>
+        <input type="text" style="height:90px;width:600px"><br><br>
+        <button style="background-color:green; color:white; margin-bottom: 10px; font-size:18px"> Submit </button>
+        
+
+    </div>
+</body>
+</html>
