a496d9cbdbd84f1b1339ac8745e2731778654106
Ankon 190 <ankon190@gmail.com>
2025-11-17 11:09:15 +0600

    Form layout creating

--- Mid/lab2.php
+++ Mid/lab2.php
@@ -8,6 +8,34 @@
     </style>
 </head>
 <body>
-
+    <div>
+        <h1>Student Registration</h1>
+        <table>
+            <tr>
+                <td>Full Name</td>
+            </tr>
+            <tr>
+                <td><input type="text"></td>
+            </tr>
+            <tr>
+                <td>Email</td>
+            </tr>
+            <tr>
+                <td><input type="email"></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+            </tr>
+            <tr>
+                <td><input type="password"></td>
+            </tr>
+            <tr>
+                <td>Confirm Password</td>
+            </tr>
+            <tr>
+                <td><input type="password"></td>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
