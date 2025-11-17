a1acdbaa601c2d5814dc31ef11c4604942db6ebf
Ankon 190 <ankon190@gmail.com>
2025-11-17 12:11:21 +0600

    Reg form validation added

--- Mid/lab2.php
+++ Mid/lab2.php
@@ -19,6 +19,11 @@
             padding: 10px;
             margin: 0 auto;
         }
+        #errorMessage{
+            color: black;
+            text-align: center;
+            margin-top: 10px;
+        }
         table {
             width: 100%;
         }
@@ -27,7 +32,8 @@
 <body>
     <div id="reg">
         <h2>Student Registration</h2>
-        <table>
+        <form onsubmit="return validation()">
+             <table>
             <tr>
                 <td>Full Name</td>
             </tr>
@@ -56,6 +62,13 @@
                 <td><input type="submit" value="Register" id="RegisterButton"></td>
             </tr>
         </table>
+        </form>
+    </div>
+    <div id="errorMessage">
+
+    </div>
+    <div id="outputMessage">
+
     </div>
     <div id="course" style="margin-top: 20px;">
         <h2>Course Registration</h2>
@@ -82,6 +95,30 @@
             let email = document.getElementById("Email").value.trim();
             let password = document.getElementById("Password").value;
             let cpassword = document.getElementById("Cpassword").value;
+
+            let errorDiv= document.getElementById("errorMessage");
+            let outputDiv = document.getElementById("outputMessage");
+
+            errorDiv.innerHTML = "";
+            outputDiv.innerHTML = "";
+
+            if(name === "" || email === "" || password === "" || cpassword === ""){
+                errorDiv.innerHTML = "Please fill in all fields with proper info.";
+                return false;
+            }
+
+            if(email.indexOf("@") === -1){
+                errorDiv.innerHTML = "Please enter a valid email address.";
+                return false;
+            }
+            if(password !== cpassword){
+                errorDiv.innerHTML = "Passwords do not match.";
+                return false;
+            }
+            outputDiv.innerHTML = '<strong>Registration successful!</strong> <br>'+
+            'Name: ' + name + '<br>' +
+            'Email: ' + email;
+            return false;
         }
     </script>
 </body>
