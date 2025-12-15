bcfcf533e3f0b8e714d4cfe655e29af6b8a4b677
Ankon190 <ankon190@gmail.com>
2025-12-15 12:07:57 +0600

    Final 1st lab task

---
+++ Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Login</title>
+    <link rel="stylesheet" href="LoginStyle.css">
+</head>
+<body>
+    <div class="wrapper">
+        <form action="">
+            <h1>Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" required>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" required>
+            </div>
+            <div class="remember-forget">
+                <label><input type="checkbox">Remember me</label>
+                <a href="#">Forgot Password?</a>
+            </div>
+            <button type="submit" class="btn">Login</button>
+            <div class="register-link">
+                <p>Don't have an account? <a href="Registerpage.php">Register</a></p>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
